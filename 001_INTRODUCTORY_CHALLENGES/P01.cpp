f5f550911c36804d059e4300d8b4dc290e330186
GuiWallace9 <103529882+GuiWallace9@users.noreply.github.com>
2023-07-03 21:57:25 -0300

    P01.cpp

--- 001_INTRODUCTORY_CHALLENGES/P01.cpp
+++ 001_INTRODUCTORY_CHALLENGES/P01.cpp
@@ -1,1 +1,25 @@
+// CODING CHALLENGES 
+// Guilherme Wallace
+// Email: guiwallace@usp.br / guiwallace96@gmail.com
+
+// You are given two integers a and b. Print a + b
+
 #include <iostream>
+
+int main(){
+
+    //Number o test cases
+    int N;
+    //Number a and b
+    int a, b;
+
+    for( int i=0; i < N; i++ ) {
+        //Read the two numbers
+        std::cin >> a >> b;
+        //Print the sum of them
+        std::cout << a+b << std::endl;
+    }
+
+    return 0;
+}
+
